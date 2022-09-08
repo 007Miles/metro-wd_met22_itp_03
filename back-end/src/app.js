f8f9e9f678d30752d46ddf33c1c2aabf8a1dab29
QbicSquid <shifan611@gmail.com>
2022-09-08 15:52:50 +0530

    Initialized express

---
+++ back-end/src/app.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
