609ac682eede1dd8e9946a553b2bd4cdacda4cf9
Kariyawasam K.P.W.D.V <it21036620@my.sliit.lk>
2022-09-11 12:07:08 +0530

    supplier Schema

---
+++ back-end/src/models/supplier.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const SupplierSchema = new mongoose.Schema(
+  {
+    business_name: {
+      required: true,
+      type: String,
+    },
+    cred_id: {
+      required: true,
+      type: String,
+    },
+    address: {
+      required: true,
+      type: String,
+    },
+    email: {
+      required: true,
+      type: String,
+    },
+    phone: [
+      {
+        required: true,
+        type: String,
+      },
+    ],
+    registered_products: [
+      {
+        required: true,
+        type: String,
+      },
+    ],
+    rating: {
+      required: false,
+      type: Number,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: 'created_at', updated: 'Updated_at' },
+  }
+)
+
+const supplier = mongoose.model('supplier', SupplierSchema)
+
+export default supplier
