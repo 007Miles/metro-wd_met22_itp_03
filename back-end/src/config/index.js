5928d016b5f6987270aedd87399243379caa95d3
DKxNK <dknalinga99@gmail.com>
2022-09-22 21:38:28 +0530

    EmployeeCRUD

--- back-end/src/config/index.js
+++
@@ -1,7 +0,0 @@
-//config db connection
-
-const config = {
-  DB_CONNECTION_STRING: process.env.MONGODB_URL,
-}
-
-export default config
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import deliveryRouter from './delivery.js'
 
 const router = express.Router()
 import employee_router from './employee.js'
@@ -8,9 +7,6 @@ import employee_router from './employee.js'
 router.use('/employee', employee_router);
 // router.use('/warehouse', warehouse_router);
 // router.use('/role', role_router);
-
-router.use('/delivery', deliveryRouter)
-
 
 export default router
 
