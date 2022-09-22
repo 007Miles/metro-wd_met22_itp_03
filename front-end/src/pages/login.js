a86003d5256bdcd2ed7b676fee672ec89f84d515
Shifan <shifan611@gmail.com>
2022-09-22 09:54:21 +0530

    Added tailwind

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,10 +1,12 @@
 import Layout from '../components/layout'
+import Button from '../components/common/button'
 
 const Login = () => {
   return (
     <Layout>
       <div className="App">
         <p>LOGIN</p>
+        <Button></Button>
       </div>
     </Layout>
   )
