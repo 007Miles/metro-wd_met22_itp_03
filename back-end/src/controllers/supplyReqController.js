b117bb67c92e054556108cbb341e7ffb2be5e670
Kariyawasam K.P.W.D.V <it21036620@my.sliit.lk>
2022-10-31 16:12:06 +0530

    MakeResponse added to controllers

--- back-end/src/controllers/supplyReqController.js
+++ back-end/src/controllers/supplyReqController.js
@@ -1,4 +1,5 @@
 import asyncHandler from '../middleware/async.js'
+import makeResponse from '../middleware/response.js'
 import {
   addSupplyReq,
   getSupplyReqById,
@@ -10,32 +11,62 @@ import {
 //Insert A New Supply Request
 export const supplyReqAdd = asyncHandler(async (req, res) => {
   const ans = await addSupplyReq(req.body)
+  return makeResponse({
+    res,
+    status: 201,
+    data: ans,
+    massage: 'Supply inspection request successfully created',
+  })
 
-  res.status(200).json(ans)
+  // res.status(200).json(ans)
 })
 
 //Get Data Of One Supply Request
 export const supplyReqGet = asyncHandler(async (req, res) => {
   const ans = await getSupplyReqById(req.params.id)
+  return makeResponse({
+    res,
+    status: 200,
+    data: ans,
+    massage: 'Single Supply inspection request successfully fetched',
+  })
 
-  res.status(200).json(ans)
+  // res.status(200).json(ans)
 })
 
 //Get Data Of All Supply Requests
 export const suppliesReqGet = asyncHandler(async (req, res) => {
   const ans = await getAllSupplyReqs()
+  return makeResponse({
+    res,
+    status: 200,
+    data: ans,
+    massage: 'Supply inspection requests successfully fetched',
+  })
 
-  res.status(200).json(ans)
+  // res.status(200).json(ans)
 })
 
 //Update Supply Request Data
 export const supplyReqUpdate = asyncHandler(async (req, res) => {
   const ans = await updateSupplyReqById(req.params.id, req.body)
-  res.send(ans)
+  return makeResponse({
+    res,
+    status: 200,
+    data: ans,
+    massage: 'Supply inspection request successfully updated',
+  })
+  // res.send(ans)
 })
 
 //Delete A Supply Request
 export const supplyReqDelete = asyncHandler(async (req, res) => {
   const ans = await deleteSupplyReqById(req.params.id)
-  res.send(ans)
+  return makeResponse({
+    res,
+    status: 200,
+    data: ans,
+    massage: 'Supply inspection request successfully created',
+  })
+  // res.send(ans)
 })
