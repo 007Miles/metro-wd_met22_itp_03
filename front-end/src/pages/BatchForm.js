813cd6c7ae724f55e07eda36681d17d669167e9c
Devshe <sheroniperera20@gmail.com>
2022-11-01 20:03:21 +0530

    Form css added

--- front-end/src/pages/BatchForm.js
+++ front-end/src/pages/BatchForm.js
@@ -57,65 +57,126 @@ const BatchForm = () => {
     }
   }
   return (
-    <form className="createProduct" onSubmit={handleSubmit}>
-      <h3> Add Batch</h3>
-      <label>Product Name</label>
-      <input
-        type="text"
-        onChange={(e) => setProd_Name(e.target.value)}
-        value={prod_Name}
-      />
-      <label>Warehouse</label>
-      <input
-        type="text"
-        onChange={(e) => setProd_warehouse_id(e.target.value)}
-        value={warehouse_id}
-      />
-      <label>Quantity</label>
-      <input
-        type="number"
-        onChange={(e) => setProd_qnty(e.target.value)}
-        value={quantity}
-      />
-      <label>Expiry Date</label>
-      <input
-        type="date"
-        onChange={(e) => setProd_expdate(e.target.value)}
-        value={exp_date}
-      />
-      <label>Manifacture Date</label>
-      <input
-        type="date"
-        onChange={(e) => setProd_ManuDate(e.target.value)}
-        value={manu_date}
-      />
-      <label>Arrived Date</label>
-      <input
-        type="date"
-        onChange={(e) => setProd_arrvDate(e.target.value)}
-        value={arrived_date}
-      />
-      <label>Supplier Name</label>
-      <input
-        type="text"
-        onChange={(e) => setProd_suppName(e.target.value)}
-        value={supplier_Name}
-      />
-      <label>Buy Price</label>
-      <input
-        type="number"
-        onChange={(e) => setProd_buyPr(e.target.value)}
-        value={buy_price}
-      />
-      <label>Sell Price</label>
-      <input
-        type="number"
-        onChange={(e) => setProd_sellPr(e.target.value)}
-        value={sell_price}
-      />
-      <button> Add Batch</button>
-      {error && <div className="error">{error}</div>}
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="border-solid px-16 border-2 border-indigo-600 ...">
+          <h3> Add Batch</h3>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 mb-6 w-full group">
+              <div className="py-3 px-6">
+                <label>Product Name</label>
+
+                <input
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={(e) => setProd_Name(e.target.value)}
+                  value={prod_Name}
+                />
+              </div>
+            </div>
+            <div className="relative z-0 mb-6 w-full group">
+              <label>Warehouse</label>
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setProd_warehouse_id(e.target.value)}
+                value={warehouse_id}
+              />
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 mb-6 w-full group">
+              <div className="py-3 px-6">
+                <label>Quantity</label>
+
+                <input
+                  type="number"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={(e) => setProd_qnty(e.target.value)}
+                  value={quantity}
+                />
+              </div>
+            </div>
+            <div className="relative z-0 mb-6 w-full group">
+              <label>Expiry Date</label>
+              <input
+                type="date"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setProd_expdate(e.target.value)}
+                value={exp_date}
+              />
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 mb-6 w-full group">
+              <div className="py-3 px-6">
+                <label>Manifacture Date</label>
+
+                <input
+                  type="date"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={(e) => setProd_ManuDate(e.target.value)}
+                  value={manu_date}
+                />
+              </div>
+            </div>
+            <div className="relative z-0 mb-6 w-full group">
+              <label>Arrived Date</label>
+              <input
+                type="date"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setProd_arrvDate(e.target.value)}
+                value={arrived_date}
+              />
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 mb-6 w-full group">
+              <div className="py-3 px-6">
+                <label>Supplier Name</label>
+
+                <input
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onChange={(e) => setProd_suppName(e.target.value)}
+                  value={supplier_Name}
+                />
+              </div>
+            </div>
+            <div className="relative z-0 mb-6 w-full group">
+              <label>Buy Price</label>
+              <input
+                type="number"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setProd_buyPr(e.target.value)}
+                value={buy_price}
+              />
+            </div>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <div className="py-3 px-6">
+              <label>Sell Price</label>
+
+              <input
+                type="number"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setProd_sellPr(e.target.value)}
+                value={sell_price}
+              />
+            </div>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <div className="py-3 px 6">
+              <button className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                {' '}
+                Add Batch
+              </button>
+              {error && <div className="error">{error}</div>}{' '}
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
