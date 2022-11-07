25877cd95d37212d629ca51b6d1aed2427cc4815
Shifan <76872645+QbicSquid@users.noreply.github.com>
2022-11-07 21:52:20 +0530

    Consistency fixes

--- back-end/src/routes/delivery.js
+++ back-end/src/routes/delivery.js
@@ -2,22 +2,11 @@ import express from 'express'
 import { celebrate, Segments } from 'celebrate'
 
 import { viewDelivery, createDelivery } from '../controllers/delivery.js'
-import {
-  createDeliverySchema,
-  viewDeliverySchema,
-} from '../validations/delivery.js'
+import { createDeliverySchema, viewDeliverySchema } from '../validations/delivery.js'
 
 const sampleRouter = express.Router()
 
-sampleRouter.post(
-  '/create',
-  celebrate({ [Segments.BODY]: createDeliverySchema }),
-  createDelivery
-)
-sampleRouter.get(
-  '/:delivery_id',
-  celebrate({ [Segments.PARAMS]: viewDeliverySchema }),
-  viewDelivery
-)
+sampleRouter.post('/create', celebrate({ [Segments.BODY]: createDeliverySchema }), createDelivery)
+sampleRouter.get('/:delivery_id', celebrate({ [Segments.PARAMS]: viewDeliverySchema }) , viewDelivery)
 
 export default sampleRouter
--- back-end/src/routes/employee.js
+++ back-end/src/routes/employee.js
@@ -1,19 +1,10 @@
 import { celebrate, Segments } from 'celebrate'
 import express from 'express'
 const router = express.Router()
-import {
-  employeeRegister,
-  getEmployee,
-  updateEmployee,
-  deleteEmployee,
-} from '../controllers/employee.js'
+import { employeeRegister, getEmployee, updateEmployee, deleteEmployee } from '../controllers/employee.js'
 import { createEmployeeSchema } from '../validations/employee.js'
 
-router.post(
-  '/registration',
-  celebrate({ [Segments.BODY]: createEmployeeSchema }),
-  employeeRegister
-) //postman link
+router.post('/registration',celebrate({[Segments.BODY]:createEmployeeSchema}), employeeRegister) //postman link
 router.get('/:id', getEmployee)
 router.put('/:id', updateEmployee)
 router.delete('/:id', deleteEmployee)
