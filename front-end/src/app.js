49476ad32a59a160a5fa3556459f7d0c6aa84853
Devshe <sheroniperera20@gmail.com>
2022-11-07 22:06:07 +0530

    App.js fixed

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,11 +1,21 @@
-import { BrowserRouter } from 'react-router-dom'
-import IndexRoutes from './routes'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductList from './pages/ProductList.js'
+import UpdateProduct from './components/UpdateProduct.js'
+import Navbar from './components/Navbar.js'
+import ProductForm from './pages/ProductForm.js'
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <IndexRoutes />
+        <Navbar />
+        <div className="product">
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/ProductForm" element={<ProductForm />} />
+            <Route path="/updateProduct/:id" element={<UpdateProduct />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   )
