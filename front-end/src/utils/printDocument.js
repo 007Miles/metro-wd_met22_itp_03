a015342f1eafeb4a39040b54251dc6073e18a592
QbicSquid <shifan611@gmail.com>
2022-11-08 22:02:06 +0530

    Printing supports page size

--- front-end/src/utils/printDocument.js
+++ front-end/src/utils/printDocument.js
@@ -1,13 +1,12 @@
 import { jsPDF } from 'jspdf'
 import html2canvas from 'html2canvas'
 
-const printDocument = (input) => {
+const printDocument = (element, size) => {
   // input must be a dom element
-  html2canvas(input).then((canvas) => {
+  html2canvas(element).then((canvas) => {
     const imgData = canvas.toDataURL('image/png')
-    const pdf = new jsPDF()
+    const pdf = new jsPDF({ format: size })
     pdf.addImage(imgData, 'JPEG', 0, 0)
-    // pdf.output('dataurlnewwindow');
     pdf.save('download.pdf')
   })
 }
