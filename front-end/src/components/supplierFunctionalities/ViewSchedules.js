58e088b5a8729d1934f1d679364abcf56689ca8f
007Miles <milendrafernando@gmail.com>
2022-11-09 02:21:44 +0530

    Slight Changes

--- front-end/src/components/supplierFunctionalities/ViewSchedules.js
+++ front-end/src/components/supplierFunctionalities/ViewSchedules.js
@@ -240,9 +240,8 @@ export default function ViewSchedules() {
         </form>
         <div>
           <button
-            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             onClick={handlePrint}
-            className="print_button"
           >
             {' '}
             Print Schedule{' '}
