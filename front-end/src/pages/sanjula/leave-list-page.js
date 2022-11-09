c828a08eda26ec88db1b2d5ec47c926b405edcd1
IT21032592 <it21032592@my.sliit.lk>
2022-11-09 09:59:58 +0530

    Search funtion working

--- front-end/src/pages/sanjula/leave-list-page.js
+++ front-end/src/pages/sanjula/leave-list-page.js
@@ -213,7 +213,15 @@ const LeaveList = () => {
                       </tbody>
                     </table>
                     <br></br>
-                    <button className='inline-block px-6 py-2 border-2 border-green-500 text-green-500 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out' onClick={() => printDocument(document.body)}> PRINT LEAVE LIST </button>
+                    <center>
+                      <button
+                        className="inline-block px-6 py-2 border-2 border-green-500 text-green-500 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                        onClick={() => printDocument(document.body)}
+                      >
+                        {' '}
+                        PRINT LEAVE LIST{' '}
+                      </button>
+                    </center>
                   </div>
                 </div>
               </div>
