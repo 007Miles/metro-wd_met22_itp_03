8123de82b054456837d87f1debc481bfc02aa28c
RivinduFernando <fernandorivindu2@gmail.com>
2022-11-09 09:42:05 +0530

    bug fixed

--- front-end/src/components/buyerAccount/products.js
+++ front-end/src/components/buyerAccount/products.js
@@ -110,7 +110,7 @@ const Products = () => {
                             scope="col"
                             class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                           >
-                            Price
+                            Prices/per Kg
                           </th>
                           <th
                             scope="col"
